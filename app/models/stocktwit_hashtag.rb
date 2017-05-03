236c00f74a813c287b80b113238e0fac1d9c9867
Winston Kotzan <wak@wakproductions.com>
2017-05-02 22:14:30 -0400

    Convenience method for deleting hashtags

--- app/models/stocktwit_hashtag.rb
+++ app/models/stocktwit_hashtag.rb
@@ -1,3 +1,9 @@
 class StocktwitHashtag < ActiveRecord::Base
   belongs_to :stocktwit
+
+  class << self
+    def rename(old_hashtag, new_hashtag)
+      self.where(tag: old_hashtag).update_all(tag: new_hashtag)
+    end
+  end
 end
