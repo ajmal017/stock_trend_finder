592f6bc012bfd1edd75caa485de4e5a04a1d5bfd
Winston Kotzan <wak@wakproductions.com>
2017-05-04 19:29:51 -0400

    Better organization of class; add population of 52 week high

--- lib/tdameritrade_data_interface/import_daily_quotes.rb
+++ lib/tdameritrade_data_interface/import_daily_quotes.rb
@@ -3,6 +3,7 @@ require 'tdameritrade_data_interface/util'
 require 'evernote/evernote_watchlist'
 
 module TDAmeritradeDataInterface
+  extend RunDaemons
   include SQLQueryStrings
 
   def self.defunct_tickers
@@ -23,7 +24,7 @@ module TDAmeritradeDataInterface
     log = ""
     #c.session_id = "128459556EEA989391FBAAA5E2BF8EB4.cOr5v8xckaAXQxWmG7bn2g"
 
-    Ticker.watching.where("symbol not like '%^%'").each.with_index(1) do |ticker, i|
+    Ticker.watching.each.with_index(1) do |ticker, i|
       #Ticker.watching.where("symbol='AAOI'").each.with_index(1) do |ticker, i|
 
       puts "Processing #{i}: #{ticker.symbol}"
@@ -103,11 +104,11 @@ module TDAmeritradeDataInterface
     puts "Updating Previous Low Cache - #{Time.now}"
     populate_previous_low(Date.today)
 
+    puts "Updating 52 Week High Cache - #{Time.now}"
+    populate_high_52_weeks(Date.today)
+
     puts "Calculating Average Daily Volumes - #{Time.now}"
     populate_average_volume_50day(NEW_TICKER_BEGIN_DATE)
-
-    #puts "Calculating EMA13's"
-    #populate_ema13
 
     puts "Calculating SMA50's - #{Time.now}"
     populate_sma50
@@ -223,6 +224,9 @@ module TDAmeritradeDataInterface
     puts "Updating Previous Low Cache - #{Time.now}"
     populate_previous_low
 
+    puts "Updating 52 Week High Cache - #{Time.now}"
+    populate_high_52_weeks
+
     puts "Calculating SMA50's - #{Time.now}"
     populate_sma50
 
@@ -262,7 +266,7 @@ module TDAmeritradeDataInterface
 
     RealTimeQuote.reset_cache
 
-    ticker_watch_list = Ticker.watching.where("symbol not like '%^%'").pluck(:symbol, :id)
+    ticker_watch_list = Ticker.watching.pluck(:symbol, :id)
     begin
       list = ticker_watch_list.slice!(0,250)
       quotes = nil
@@ -538,163 +542,9 @@ module TDAmeritradeDataInterface
     ActiveRecord::Base.connection.execute "VACUUM ANALYZE"
   end
 
-  def self.realtime_quote_daemon_block
-    puts "Real Time Quote Import: #{Time.now}"
-    if is_market_day? Date.today
-      ActiveRecord::Base.connection_pool.with_connection do
-        import_realtime_quotes
-        copy_realtime_quotes_to_daily_stock_prices
-      end
-      puts "Done #{Time.now}\n\n"
-    else
-      puts "Market closed today, no real time quote download necessary"
-    end
+  def self.populate_high_52_weeks(begin_date=NEW_TICKER_BEGIN_DATE)
+    ActiveRecord::Base.connection.execute update_high_52_week(begin_date)
   end
-
-  def self.run_realtime_quotes_daemon
-    scheduler = Rufus::Scheduler.new
-    scheduler.cron('15,45 10-15 * * MON-FRI') { realtime_quote_daemon_block }
-    scheduler2 = Rufus::Scheduler.new
-    scheduler2.cron('45 9 * * MON-FRI') { realtime_quote_daemon_block }
-    puts "#{Time.now} Beginning realtime quote import daemon..."
-    [scheduler, scheduler2]
-  end
-
-  def self.run_daily_quotes_daemon
-    scheduler = Rufus::Scheduler.new
-    scheduler.cron('10 16 * * MON-FRI') do
-      puts "Daily Quote Import: #{Time.now}"
-      if is_market_day? Date.today
-        ActiveRecord::Base.connection_pool.with_connection do
-          update_daily_stock_prices_from_real_time_snapshot
-        end
-      else
-        puts "Market closed today, no real time quote download necessary"
-      end
-    end
-    puts "#{Time.now} Beginning daily quotes update daemon..."
-    scheduler
-  end
-
-  def self.run_prepopulate_daily_stock_quotes_daemon
-    scheduler = Rufus::Scheduler.new
-    scheduler.cron('12 6 * * MON-FRI') do
-      puts "Prepopulating daily_stock_quotes table: #{Time.now}"
-      ActiveRecord::Base.connection_pool.with_connection do
-        prepopulate_daily_stock_prices(Date.today)
-      end
-    end
-    puts "#{Time.now} Beginning daily_stock_prices prepopulate daemon..."
-    scheduler
-  end
-
-  def self.run_premarket_quotes_daemon
-    scheduler = Rufus::Scheduler.new
-    scheduler.cron('4,25,40,59 8 * * MON-FRI') do
-      puts "Premarket Quote Import: #{Time.now}"
-      if is_market_day? Date.today
-        ActiveRecord::Base.connection_pool.with_connection do
-          import_premarket_quotes(date: Date.today)
-        end
-      else
-        puts "Market closed today, no real time quote download necessary"
-      end
-    end
-    puts "#{Time.now} Beginning premarket quotes update daemon..."
-    scheduler
-  end
-
-  def self.run_premarket_memoization_daemon
-    scheduler = Rufus::Scheduler.new
-    scheduler.cron('0 5 * * MON-FRI') do
-      puts "Memoizing premarket high, low, close, average volume - #{Time.now}"
-      if is_market_day? Date.today
-        ActiveRecord::Base.connection_pool.with_connection do
-          populate_premarket_memoized_fields(Date.today)
-        end
-      else
-        puts "Market closed today, no real time quote download necessary"
-      end
-    end
-    puts "#{Time.now} Beginning premarket calculations memoization daemon..."
-    scheduler
-  end
-
-  def self.run_afterhours_quotes_daemon
-    scheduler = Rufus::Scheduler.new
-    scheduler.cron('0 17,18,19,21 * * MON-FRI') do
-      puts "Afterhours Quote Import: #{Time.now}"
-      if is_market_day? Date.today
-        ActiveRecord::Base.connection_pool.with_connection do
-          import_afterhours_quotes(date: Date.today)
-        end
-      else
-        puts "Market closed today, no real time quote download necessary"
-      end
-    end
-    puts "#{Time.now} Beginning afterhours quotes update daemon..."
-    scheduler
-  end
-
-  def self.run_stocktwits_sync_daemon
-    scheduler = Rufus::Scheduler.new
-    scheduler.cron('0 0,7,16 * * *') do
-      puts "StockTwits data sync: #{Time.now}"
-      ActiveRecord::Base.connection_pool.with_connection do
-        Stocktwit.sync_twits
-      end
-    end
-    puts "#{Time.now} Beginning StockTwits sync daemon..."
-    scheduler
-  end
-
-  def self.run_import_vix_futures_daemon
-    scheduler = Rufus::Scheduler.new
-    scheduler.cron('0 9,10,14,17 * * MON-FRI') do
-      puts "VIX Futures data sync: #{Time.now}"
-      ActiveRecord::Base.connection_pool.with_connection do
-        VIXFuturesHistory.import_vix_futures if is_market_day?(Date.today)
-      end
-      puts "Done"
-    end
-    puts "#{Time.now} Beginning VIX Futures History daemon..."
-    scheduler
-  end
-
-  def self.run_db_maintenance_daemon
-    scheduler = Rufus::Scheduler.new
-    scheduler.cron('0 1 * * SUN-FRI') do
-      puts "Running DB VACUUM: #{Time.now}"
-      ActiveRecord::Base.connection_pool.with_connection do
-        ActiveRecord::Base.connection.execute "VACUUM FULL"
-        ActiveRecord::Base.connection.execute "VACUUM ANALYZE"
-      end
-      puts "Done"
-    end
-
-    scheduler_rts = Rufus::Scheduler.new
-    scheduler_rts.cron('0 1 * * SAT') do
-      puts "Resetting Realtime Snapshot Flags #{Time.now}"
-      ActiveRecord::Base.connection_pool.with_connection do
-        $stf.reset_snapshot_flags
-      end
-      puts "Done"
-    end
-    puts "#{Time.now} Beginning DB Maintenance daemon..."
-    scheduler
-  end
-
-  def self.run_evernote_watchlist_daemon
-    scheduler = Rufus::Scheduler.new
-    scheduler.cron('45 1 * * *') do
-      puts "Building Evernote Watchlist #{Time.now}"
-      Evernote::EvernoteWatchList.build_evernote_watchlist
-      puts "Done building Evernote Watchlist"
-    end
-    puts "#{Time.now} Beginning Evernote watchlist daemon..."
-    scheduler
-  end
-
 
   def self.populate_previous_close(begin_date=NEW_TICKER_BEGIN_DATE)
     ActiveRecord::Base.connection.execute update_previous_close(begin_date)
@@ -836,6 +686,7 @@ module TDAmeritradeDataInterface
 
 
   private
+  
   def self.get_history_returned_error?(return_value)
     if return_value.is_a? Array and return_value.length > 0
       return return_value.first.has_key?(:error)
