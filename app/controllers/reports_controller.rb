ee4456159f07a5d1c3fc57d1ab2351feb010631d
Winston Kotzan <wak@wakproductions.com>
2017-05-09 08:50:14 -0400

    Refactor report presentation

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,7 +2,62 @@ require 'tdameritrade_data_interface/tdameritrade_data_interface'
 
 class ReportsController < ApplicationController
   before_filter :set_report_date
-  before_filter :set_vix_contango_reading
+  # before_filter :set_vix_contango_reading
+
+  def active_stocks
+    @fields = [:ticker_symbol, :last_trade, :pct_change, :volume, :average_volume, :volume_ratio, :short_ratio, :float, :float_pct, :actions]
+    @report = run_query(
+      TDAmeritradeDataInterface.select_active_stocks(@report_date),
+      @fields
+    )
+
+    @report_up   = @report.select { |r| r[:pct_change].to_f >= 0 }
+    @report_down = @report.select { |r| r[:pct_change].to_f < 0  }
+  end
+
+  def afterhours
+    @fields = [:ticker_symbol, :last_trade, :pct_change, :volume, :average_volume, :volume_ratio, :short_ratio, :float, :float_pct, :actions]
+
+    @report_volume = run_query(
+      TDAmeritradeDataInterface.select_afterhours_by_volume(@report_date),
+      @fields,
+    )
+    @report_percent = run_query(
+      TDAmeritradeDataInterface.select_afterhours_by_percent(@report_date),
+      @fields
+    )
+
+    @report_volume_up = @report_volume.select { |r| r[:pct_change].to_f >= 0 }
+    @report_volume_down = @report_volume.select { |r| r[:pct_change].to_f < 0 }
+  end
+
+  def gaps
+    @fields = [:ticker_symbol, :last_trade, :high, :pct_change, :gap_percent, :volume, :average_volume, :volume_ratio, :short_ratio, :float, :float_pct, :actions]
+
+    @report_bullgaps = run_query(
+      TDAmeritradeDataInterface.select_bullish_gaps(@report_date),
+      @fields,
+    )
+    @report_beargaps = run_query(
+      TDAmeritradeDataInterface.select_bearish_gaps(@report_date),
+      @fields,
+    )
+  end
+
+  def premarket
+    @fields = [:ticker_symbol, :last_trade, :pct_change, :volume, :average_volume, :volume_ratio, :short_ratio, :float, :float_pct, :actions]
+    @report_volume = run_query(
+      TDAmeritradeDataInterface.select_premarket_by_volume(@report_date),
+      @fields,
+    )
+    @report_percent = run_query(
+      TDAmeritradeDataInterface.select_premarket_by_percent(@report_date),
+      @fields,
+    )
+
+    @report_volume_up = @report_volume.select { |r| r[:pct_change].to_f >= 0 }
+    @report_volume_down = @report_volume.select { |r| r[:pct_change].to_f < 0 }
+  end
 
   def hide_symbol
     @symbol = params[:symbol]
@@ -14,95 +69,61 @@ class ReportsController < ApplicationController
     Ticker.unscrape(@symbol)
   end
 
-  def range
-    @report = run_query(TDAmeritradeDataInterface.select_big_range(@report_date))
-  end
-
-  def ipo_list
-    @report = run_query(TDAmeritradeDataInterface.select_ipo_list)
-  end
-
-  def gaps
-    @report_bullgaps = run_query(TDAmeritradeDataInterface.select_bullish_gaps(@report_date))
-    @report_beargaps = run_query(TDAmeritradeDataInterface.select_bearish_gaps(@report_date))
-  end
-
-  def ema13_breaks
-    @report = run_query(TDAmeritradeDataInterface.select_ema13_bullish_breaks)
-  end
-
-  def sma50_breaks
-    @report_bull50sma = run_query(TDAmeritradeDataInterface.select_sma50_bull_cross(@report_date))
-    @report_bear50sma = run_query(TDAmeritradeDataInterface.select_sma50_bear_cross(@report_date))
-  end
-
-  def sma200_breaks
-    @report_bull200sma = run_query(TDAmeritradeDataInterface.select_sma200_bull_cross(@report_date))
-    @report_bear200sma = run_query(TDAmeritradeDataInterface.select_sma200_bear_cross(@report_date))
-  end
-
   def week52_highs
-    @report = run_query(TDAmeritradeDataInterface.select_52week_highs(@report_date))
+    @fields = [:ticker_symbol, :last_trade, :pct_above_52, :volume, :average_volume, :volume_ratio, :short_ratio, :float, :float_pct, :actions]
+    @report = run_query(
+      TDAmeritradeDataInterface.select_52week_highs(@report_date),
+      @fields,
+    )
   end
+
+  # def range
+  #   @report = run_query(TDAmeritradeDataInterface.select_big_range(@report_date))
+  # end
+  #
+  # def ipo_list
+  #   @report = run_query(TDAmeritradeDataInterface.select_ipo_list)
+  # end
+  #
+  # def ema13_breaks
+  #   @report = run_query(TDAmeritradeDataInterface.select_ema13_bullish_breaks)
+  # end
+  #
+  # def sma50_breaks
+  #   @report_bull50sma = run_query(TDAmeritradeDataInterface.select_sma50_bull_cross(@report_date))
+  #   @report_bear50sma = run_query(TDAmeritradeDataInterface.select_sma50_bear_cross(@report_date))
+  # end
+  #
+  # def sma200_breaks
+  #   @report_bull200sma = run_query(TDAmeritradeDataInterface.select_sma200_bull_cross(@report_date))
+  #   @report_bear200sma = run_query(TDAmeritradeDataInterface.select_sma200_bear_cross(@report_date))
+  # end
 
   def ticker_list
     @report = Ticker.watching.order(id: :desc)
   end
 
-  def hammers
-    @report = run_query(TDAmeritradeDataInterface.select_hammers)
-  end
+  # def hammers
+  #   @report = run_query(TDAmeritradeDataInterface.select_hammers)
+  # end
 
-  def active_stocks
-    @report = run_query(TDAmeritradeDataInterface.select_active_stocks(@report_date))
-
-    @report_up   = @report.select { |r| r['pct_change'].to_f >= 0 }
-    @report_down = @report.select { |r| r['pct_change'].to_f < 0  }
-  end
-
-  def candle_row
-    @report_winners = run_query(TDAmeritradeDataInterface.select_4_green_candles(@report_date))
-    @report_losers = run_query(TDAmeritradeDataInterface.select_4_red_candles(@report_date))
-
-  end
-
+  # def candle_row
+  #   @report_winners = run_query(TDAmeritradeDataInterface.select_4_green_candles(@report_date))
+  #   @report_losers = run_query(TDAmeritradeDataInterface.select_4_red_candles(@report_date))
+  #
+  # end
+  #
   def pctgainloss
     @report_winners = run_query(TDAmeritradeDataInterface.select_10pct_gainers(@report_date))
     @report_losers = run_query(TDAmeritradeDataInterface.select_10pct_losers(@report_date))
   end
 
-  def premarket
-    @report_volume = run_query(TDAmeritradeDataInterface.select_premarket_by_volume(@report_date))
-    @report_percent = run_query(TDAmeritradeDataInterface.select_premarket_by_percent(@report_date))
-
-    @report_volume_up = @report_volume.select { |r| r['pct_change'].to_f >= 0 }
-    @report_volume_down = @report_volume.select { |r| r['pct_change'].to_f < 0 }
-  end
-
-  def afterhours
-    @report_volume = run_query(TDAmeritradeDataInterface.select_afterhours_by_volume(@report_date))
-    @report_percent = run_query(TDAmeritradeDataInterface.select_afterhours_by_percent(@report_date))
-
-    @report_volume_up = @report_volume.select { |r| r['pct_change'].to_f >= 0 }
-    @report_volume_down = @report_volume.select { |r| r['pct_change'].to_f < 0 }
-  end
-
-  # What I want on earnings report:
-  # HEADING: DATE / BEFORE_OR_AFTER MARKET
-  # - Ticker
-  # - Name
-  # - Category
-  # - Last Price
-  # - Average Daily Volume
-  # - Float
-  # - Short interest (ratio/% of float)
-  def earnings
-    @report = build_upcoming_earnings_report
-  end
-
 private
-  def run_query(qry)
-    ActiveRecord::Base.connection.execute qry
+  def run_query(qry, fields=nil)
+    ReportPresenter.format(
+      ActiveRecord::Base.connection.execute(qry),
+      fields
+    )
   end
 
   def set_report_date
@@ -115,47 +136,5 @@ private
 
   def set_vix_contango_reading
     @vix = VIXFuturesHistory.last
-  end
-
-  def build_upcoming_earnings_report
-    # TODO this report should be a function of the model
-    @report = []
-    EarningsDay
-        .where("earnings_date <= ?", @report_date + 1)
-        .order(earnings_date: :desc, before_the_open: :asc)
-        .first(6)
-        .each do |earnings_day|
-          report_group = {
-              earnings_date: earnings_day.earnings_date,
-              before_after: earnings_day.before_the_open? ? "Before the Open" : "After the Close",
-              data: []
-          }
-          ticker_list = earnings_day.tickers.split(',')
-          tickers = Ticker.where(symbol: ticker_list, scrape_data: true).order(:symbol)
-          last_stock_prices = DailyStockPrice
-                              .where(ticker_symbol: ticker_list, price_date: DailyStockPrice.most_recent_date)
-
-          tickers.each do |ticker|
-            dsp = last_stock_prices.select { |lsp| lsp.ticker_symbol==ticker.symbol }.try(:first)
-
-            ticker_properties={}
-            ticker_properties['ticker_symbol'] = ticker.symbol
-            ticker_properties['company_name'] = ticker.company_name
-            ticker_properties['category'] = '' # I haven't created category tags yet
-            ticker_properties['last_trade'] = dsp.try(:close).try(:to_s)
-            ticker_properties['average_volume'] = dsp.try(:average_volume_50day).try(:to_s)
-            ticker_properties['float'] = ticker.float.to_s
-            ticker_properties['short_ratio'] = ticker.short_ratio.try(:round, 2).to_s  # Not implemented yet
-            if ticker.short_pct_float
-              ticker_properties['short_pct_float'] = format("%.0f", ticker.short_pct_float * 100)  # Not implemented yet
-            else
-              ticker_properties['short_pct_float'] = ""
-            end
-
-            report_group[:data] << ticker_properties
-          end
-          @report << report_group
-    end
-    @report
   end
 end
--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -1,5 +1,5 @@
 module ReportHelper
-  RJUST_FIELDS=[:last_trade, :pct_change, :gap_percent, :float, :float_percent, :volume, :volume_ratio, :average_volume, :short_ratio]
+  RJUST_FIELDS=[:last_trade, :pct_change, :pct_above_52, :gap_percent, :float, :float_percent, :volume, :volume_ratio, :average_volume, :short_ratio]
 
   def report_date_form(page)
     s = form_tag "/reports/#{page}", method: :get, authenticity_token: false do
@@ -17,52 +17,67 @@ module ReportHelper
   # TODO move this method into the presenter
   def set_css_class(report, field)
     css_class = "monospaced "
-    if report['pct_change'].to_f > 0
+    if report[:pct_change].to_f > 0
       css_class << "green "
-    elsif report['pct_change'].to_f < 0
+    elsif report[:pct_change].to_f < 0
       css_class << "red "
     end
 
-    if report['gap_pct'].to_f > 0
+    if report[:gap_pct].to_f > 0
       css_class << "green "
-    elsif report['gap_pct'].to_f < 0
+    elsif report[:gap_pct].to_f < 0
+      css_class << "red "
+    end
+
+    if report[:pct_above_52].to_f > 0
+      css_class << "green "
+    elsif report[:pct_above_52].to_f < 0
       css_class << "red "
     end
 
     css_class << "rjust " if RJUST_FIELDS.include?(field)
 
     case field
-      when :pct_change
-        if report['pct_change'].to_f < -7.5
-          css_class << "darkred-bg  "
-        elsif report['pct_change'].to_f > 7.5
+      when :pct_above_52
+        if report[:pct_above_52].to_f > 5.0
           css_class << "darkgreen-bg  "
         end
 
-        if report['pct_change'].to_f.abs > 10
+        if report[:pct_above_52].to_f.abs > 7.5
+          css_class << "bold "
+        end
+
+      when :pct_change
+        if report[:pct_change].to_f < -7.5
+          css_class << "darkred-bg  "
+        elsif report[:pct_change].to_f > 7.5
+          css_class << "darkgreen-bg  "
+        end
+
+        if report[:pct_change].to_f.abs > 10
           css_class << "bold "
         end
       when :gap_percent
-        if report['gap_pct'].to_f < -7.5
+        if report[:gap_pct].to_f < -7.5
           css_class << "darkred-bg  "
-        elsif report['gap_pct'].to_f > 7.5
+        elsif report[:gap_pct].to_f > 7.5
           css_class << "darkgreen-bg  "
         end
 
-        if report['gap_pct'].to_f.abs > 10
+        if report[:gap_pct].to_f.abs > 10
           css_class << "bold "
         end
-      when :float then
+      when :float
         # Format how it is displayed into millions from thousands of shares
-        report['float'] = '%.2f' % (BigDecimal(report['float']) / 1000).truncate(2) if report['float'].present?
+        report[:float] = '%.0f' % (BigDecimal(report[:float]) / 1000).truncate(2) if report[:float].present?
 
-        if (report['float'.to_f < 15] && (report['float'].to_f) > 0)
-          css_class << 'darkred-bg '
-        elsif (report['float'].to_f < 100) && (report['float'].to_f > 0)
-          css_class << "gold-bg "
+        if (report[:float].to_f < 15 && (report[:float].to_f) > 0)
+          css_class << 'gold-bg '
         end
-      when :volume_ratio then css_class << "yellow-bg " if (report['volume_ratio'].to_f > 3)
-      when :range then css_class << "yellow-bg " if (report['range'].to_f > 7)
+      when :float_pct
+        
+      when :volume_ratio then css_class << "yellow-bg " if (report[:volume_ratio].to_f > 3)
+      when :range then css_class << "yellow-bg " if (report[:range].to_f > 7)
       when :unscrape then css_class << "clickable "
 
     end
