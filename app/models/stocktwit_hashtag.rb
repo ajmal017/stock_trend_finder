3432760e9e47dc8dce4a7dbcaeaf9dc9726dbb5f
Winston Kotzan <wak@wakproductions.com>
2017-05-12 12:18:17 -0400

    Improve Stocktwit hashtag renaming

--- app/models/stocktwit_hashtag.rb
+++ app/models/stocktwit_hashtag.rb
@@ -3,7 +3,13 @@ class StocktwitHashtag < ActiveRecord::Base
 
   class << self
     def rename(old_hashtag, new_hashtag)
-      self.where(tag: old_hashtag).update_all(tag: new_hashtag)
+      hashtags_to_rename = self.where(tag: old_hashtag)
+      ActiveRecord::Base.transaction do
+        hashtags_to_rename.each do |h|
+          h.stocktwit.update(message: h.stocktwit.message.gsub(old_hashtag, new_hashtag))
+        end
+        hashtags_to_rename.update_all(tag: new_hashtag)
+      end
     end
   end
 end
