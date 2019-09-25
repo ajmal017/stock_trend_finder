1753f5d0021908306e2d77a899f401e5afffd131
Winston Kotzan <wak@wakproductions.com>
2019-09-25 10:07:08 -0400

    Improve sorting on 52 week high

--- lib/reports/presenters/line_item_sort.rb
+++ lib/reports/presenters/line_item_sort.rb
@@ -9,16 +9,25 @@ module Reports
 
       def call
         if sort_direction==:desc
-          line_items.sort { |li_a, li_b| (li_b[sort_field] || 0)<=>(li_a[sort_field] || 0) }
+          line_items.sort { |li_a, li_b| (fv(li_b[sort_field]) || 0)<=>(fv(li_a[sort_field]) || 0) }
         else
-          line_items.sort { |li_a, li_b| (li_a[sort_field] || 0)<=>(li_b[sort_field] || 0) }
+          line_items.sort { |li_a, li_b| (fv(li_a[sort_field]) || 0)<=>(fv(li_b[sort_field]) || 0) }
         end
       end
 
       private
 
+      # formatted value
+      def fv(value)
+        is_number? ? value&.to_f : value
+      end
+
       def sort_direction
         @sort_direction || :asc
+      end
+
+      def is_number?
+        [:week_52_streak].include?(sort_field)
       end
 
     end
